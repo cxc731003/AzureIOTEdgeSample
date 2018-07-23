aee8a343499219abd2f7b435bc287916d709591b
stevenlian <kingliantop@gmail.com>
2018-07-23 15:56:04 +0800

    IOT Edge Python samples

---
+++ PythonDeviceSolution/modules/PythonDeviceModule/main.py
@@ -0,0 +1,118 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for
+# full license information.
+
+import os
+import json
+import random
+import time
+import sys
+import iothub_client
+from iothub_client import IoTHubModuleClient, IoTHubClientError, IoTHubTransportProvider
+from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError
+
+# messageTimeout - the maximum time in milliseconds until a message times out.
+# The timeout period starts at IoTHubModuleClient.send_event_async.
+# By default, messages do not expire.
+MESSAGE_TIMEOUT = 10000
+
+# Define the JSON message to send to IoT Hub.
+TEMPERATURE = 20.0
+HUMIDITY = 60
+MSG_TXT = "{\"temperature\": %.2f,\"humidity\": %.2f}"
+
+# global counters
+RECEIVE_CALLBACKS = 0
+SEND_CALLBACKS = 0
+TWIN_CALLBACKS = 0
+
+# Choose HTTP, AMQP or MQTT as transport protocol.  Currently only MQTT is supported.
+PROTOCOL = IoTHubTransportProvider.MQTT
+
+# Callback received when the message that we're forwarding is processed.
+def send_confirmation_callback(message, result, user_context):
+    global SEND_CALLBACKS
+    print ( "Confirmation[%d] received for message with result = %s" % (user_context, result) )
+    map_properties = message.properties()
+    key_value_pair = map_properties.get_internals()
+    print ( "    Properties: %s" % key_value_pair )
+    SEND_CALLBACKS += 1
+    print ( "    Total calls confirmed: %d" % SEND_CALLBACKS )
+
+# module_twin_callback is invoked when twin's desired properties are updated.
+def module_twin_callback(update_state, payload, user_context):
+    global TWIN_CALLBACKS
+    print ( "\nTwin callback called with:\nupdateStatus = %s\npayload = %s\ncontext = %s" % (update_state, payload, user_context) )
+    TWIN_CALLBACKS += 1
+    print ( "Total calls confirmed: %d\n" % TWIN_CALLBACKS )
+
+
+class HubManager(object):
+
+    def __init__(
+            self,
+            protocol=IoTHubTransportProvider.MQTT):
+        self.client_protocol = protocol
+        self.client = IoTHubModuleClient()
+        self.client.create_from_environment(protocol)
+
+        # set the time until a message times out
+        self.client.set_option("messageTimeout", MESSAGE_TIMEOUT)
+        self.set_certificates()
+
+        # sets the callback when a twin's desired properties are updated.
+        self.client.set_module_twin_callback(module_twin_callback, self)
+        
+    # Set the certifications,Steven Lian,2018    
+    def set_certificates(self):
+        isWindows = sys.platform.lower() in ['windows', 'win32']
+        if not isWindows:
+            CERT_FILE = os.environ['EdgeModuleCACertificateFile']        
+            print("Adding TrustedCerts from: {0}".format(CERT_FILE))
+            
+            # this brings in x509 privateKey and certificate
+            file = open(CERT_FILE)
+            try:
+                self.client.set_option("TrustedCerts", file.read())
+                print ( "set_option TrustedCerts successful" )
+            except IoTHubClientError as iothub_client_error:
+                print ( "set_option TrustedCerts failed (%s)" % iothub_client_error )
+
+            file.close()
+    # Forwards the message received onto the next stage in the process.
+    def forward_event_to_output(self, outputQueueName, event, send_context):
+        self.client.send_event_async(
+            outputQueueName, event, send_confirmation_callback, send_context)
+    # send me
+
+def main(protocol):
+    try:
+        print ( "\nPython %s\n" % sys.version )
+        print ( "IoT Edge Client for Python" )
+
+        hub_manager = HubManager(protocol)
+
+        print ( "Starting the IoT Hub Python sample using protocol %s..." % hub_manager.client_protocol )
+        print ( "The sample is now waiting for messages and will indefinitely.  Press Ctrl-C to exit. ")
+
+        while True:
+            # Build the message with simulated telemetry values.
+            temperature = TEMPERATURE + (random.random() * 15)
+            humidity = HUMIDITY + (random.random() * 20)
+            msg_txt_formatted = MSG_TXT % (temperature, humidity)
+            message = IoTHubMessage(msg_txt_formatted)
+
+            # Send the message.
+            print( "Steven demo sending message: %s" % message.get_string() )
+            hub_manager.forward_event_to_output("temperatureOutput", message, 0)
+
+            time.sleep(1)
+
+    except IoTHubError as iothub_error:
+        print ( "Unexpected error %s from IoTHub" % iothub_error )
+        return
+    except KeyboardInterrupt:
+        print ( "IoTHubModuleClient sample stopped" )
+
+if __name__ == '__main__':
+    main(PROTOCOL)
